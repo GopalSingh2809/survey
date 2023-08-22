93dfa45c6793ff87958eb407b1e4f9ce38e88374
Gopal <gss428860@gmail.com>
2023-08-22 08:51:09 -0800

    survey_feedback done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Contact from "./components/contact";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
